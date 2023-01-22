2007c64a364d70a9e87ae1f6e1a6db7bdceb6b9a
Jude <jude.scarbrough@gmail.com>
2023-01-21 17:30:09 -0700

    just a save

--- twine/userdashboard/dash.js
+++ twine/userdashboard/dash.js
@@ -1,14 +1,3 @@
-userData = JSON.parse(localStorage.getItem("userData"))
-
-if(userData["phoneVerified"] == "no"){
-    window.location.href = "../verify/verify.html"
+if(JSON.parse(localStorage.getItem("userData"))["phoneVerified"] == "no"){
+    window.location.href="../verify/verify.html"
 }
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("pp").innerHTML = localStorage.getItem("name") + " " + localStorage.getItem("phoneNumber") + " " + localStorage.getItem("userData")
-});
-
