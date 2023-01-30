29774fee6b0e1c5a28b2da6c5be01a0b9b62a4fe
JudeScarbrough <Jude.scarbrough@gmail.com>
2023-01-30 13:02:18 -0700

    change group name and desc

--- server/editGroup.py
+++ server/editGroup.py
@@ -1,5 +1,7 @@
 import mysql.connector
 
+
+# mint
 def changeName(data):
     groupID = data["groupID"]
     newName = data["newName"]
@@ -13,7 +15,7 @@ def changeName(data):
     cursor = cnx.cursor()
 
     # Insert data into the users table
-    query = f"UPDATE groups SET groupName = '{ newName }' WHERE id = { groupID };"
+    query = f'UPDATE `groups` SET groupName = "{ newName }" WHERE groupID = { groupID };'
     cursor.execute(query)
 
     # Commit the changes to the database
@@ -26,7 +28,33 @@ def changeName(data):
     return {"Verdict": "success"}
 
 
-print(changeName({
+def changeDesc(data):
+    groupID = data["groupID"]
+    newDesc = data["newDesc"]
+
+    # Connect to the database
+    cnx = mysql.connector.connect(user='admin',
+                                  password='Password',
+                                  host='twinedb.ch3d33yazhdx.us-west-2.rds.amazonaws.com',
+                                  database='Twine_Users')
+    # Create a cursor object
+    cursor = cnx.cursor()
+
+    # Insert data into the users table
+    query = f'UPDATE `groups` SET groupDescription = "{ newDesc }" WHERE groupID = { groupID };'
+    cursor.execute(query)
+
+    # Commit the changes to the database
+    cnx.commit()
+
+    # Close the cursor and connection
+    cursor.close()
+    cnx.close()
+
+    return {"Verdict": "success"}
+
+
+print(changeDesc({
     "groupID": "1",
-    "newName": "Jude Poopy"
+    "newDesc": "jude does poopy"
 }))
